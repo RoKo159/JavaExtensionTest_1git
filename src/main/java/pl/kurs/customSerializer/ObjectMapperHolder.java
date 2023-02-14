db0cf3083211c77daf4778a4a65368df6b4b7339
RoKo159 <kowalski.robert159@gmail.com>
2023-02-14 20:55:29 +0100

    fix

--- src/main/java/pl/kurs/customSerializer/ObjectMapperHolder.java
+++ src/main/java/pl/kurs/customSerializer/ObjectMapperHolder.java
@@ -3,11 +3,6 @@ package pl.kurs.customSerializer;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import pl.kurs.models.Circle;
-import pl.kurs.models.Rectangle;
-import pl.kurs.models.Shape;
-import pl.kurs.models.Square;
 
 import java.text.SimpleDateFormat;
 
@@ -33,40 +28,6 @@ public enum ObjectMapperHolder {
 
         mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
 
-        SquareSerializer squareSerializer = new SquareSerializer(Square.class);
-        SimpleModule sm1 = new SimpleModule("SquareSerializer");
-        sm1.addSerializer(Square.class, squareSerializer);
-        mapper.registerModule(sm1);
-
-        CircleSerializer circleSerializer = new CircleSerializer(Circle.class);
-        SimpleModule sm2 = new SimpleModule("CircleSerializer");
-        sm2.addSerializer(Circle.class, circleSerializer);
-        mapper.registerModule(sm2);
-
-        RectangleSerializer rectangleSerializer = new RectangleSerializer(Rectangle.class);
-        SimpleModule sm3 = new SimpleModule("RectangleSerializer");
-        sm3.addSerializer(Rectangle.class, rectangleSerializer);
-        mapper.registerModule(sm3);
-
-        SquareDeserializer squareDeserializer = new SquareDeserializer(Square.class);
-        SimpleModule sm4 = new SimpleModule("SquareDeserializer");
-        sm4.addDeserializer(Square.class, squareDeserializer);
-        mapper.registerModule(sm4);
-
-        RectangleDeserializer rectangleDeserializer = new RectangleDeserializer(Rectangle.class);
-        SimpleModule sm5 = new SimpleModule("RectangleDeserializer");
-        sm5.addDeserializer(Rectangle.class, rectangleDeserializer);
-        mapper.registerModule(sm5);
-
-        CircleDeserializer circleDeserializer = new CircleDeserializer(Circle.class);
-        SimpleModule sm6 = new SimpleModule("CircleDeserializer");
-        sm6.addDeserializer(Circle.class, circleDeserializer);
-        mapper.registerModule(sm6);
-
-        ShapeDeserializer shapeDeserializer = new ShapeDeserializer(Shape.class);
-        SimpleModule sm7 = new SimpleModule("ShapeDeserializer");
-        sm7.addDeserializer(Shape.class, shapeDeserializer);
-        mapper.registerModule(sm7);
 
         return mapper;
     }
